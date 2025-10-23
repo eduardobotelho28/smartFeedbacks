2974e0dede949a157dd2cf5232b7733f8f6a835e
Eduardo Botelho <126706543+eduardobotelho28@users.noreply.github.com>
2025-10-23 19:52:35 -0300

    new favicon

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="<?= base_url("assets/css/styles.css") ?>">
     <link rel="stylesheet" href="<?= base_url("assets/css/mediaQueries.css") ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="icon" href="<?= base_url('favicon.ico') ?>">
+
 </head>
 
 <!-- Toast container -->
